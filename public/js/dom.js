03ee5501149a9edf130b99dee4e5d7cbfbc7693a
Osama-you <osama.w.younis@gmail.com>
2020-12-09 18:41:24 +0200

    front end Relates #16

---
+++ public/js/dom.js
@@ -0,0 +1,33 @@
+const goToHome = (name) => {
+  const container = document.querySelector('.container');
+  const cardAlt = document.querySelector(' .alt');
+  container.removeChild(cardAlt);
+
+  const buttonContainer = document.querySelector('.button-container button span');
+  buttonContainer.textContent = 'Short it';
+  document.querySelector('#label').value = '';
+
+  const inputContainer = document.querySelector('.input-container label');
+  inputContainer.textContent = 'Enter you url';
+
+  const title = document.querySelector('.title');
+  title.textContent = name;
+  const hUrl = document.createElement('h1');
+  hUrl.classList.add('url-title');
+  document.querySelectorAll('.card').forEach((node, i) => {
+    node.classList.add('card-home');
+    i == 0 || node.appendChild(hUrl);
+  });
+  document.querySelector('.container').classList.add('container-home');
+};
+
+const addUrlShort = (url) => {
+  const urlTitle = document.querySelector('.url-title');
+  urlTitle.textContent = 'your short url: ';
+  const a = document.createElement('a');
+  a.textContent = url;
+  urlTitle.appendChild(a);
+};
+
+// goToHome('osama');
+// addUrlShort('https://www.google.com/');
