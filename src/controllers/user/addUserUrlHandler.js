ac2fa8472e8c4a3f384d19cc1dcd95faa1bf4116
Osama-you <osama.w.younis@gmail.com>
2020-12-10 11:24:37 +0200

    add fun that checks if the urlShort is duplicates

--- src/controllers/user/addUserUrlHandler.js
+++ src/controllers/user/addUserUrlHandler.js
@@ -1,5 +1,5 @@
 const { boomify, createUrl } = require('../../utils');
-const { getUserId, addUrlData } = require('../../database/queries');
+const { getUserId, addUrlData, checkUrlInDB } = require('../../database/queries');
 
 const addUserUrlHandler = (req, res, next) => {
   try {
@@ -13,20 +13,34 @@ const addUserUrlHandler = (req, res, next) => {
     }
 
     const SHORT_URL_LENGTH = 7;
-    const urlShort = createUrl(SHORT_URL_LENGTH);
-    const visitors = 0;
+    createUrl(SHORT_URL_LENGTH, checkUrlInDB).then((urlShort) => {
+      const visitors = 0;
+      getUserId(name).then((data) => {
+        const userId = data.rows[0].id;
+        return addUrlData({
+          urlFull, urlShort, visitors, userId,
+        });
+      }).then(() => {
+        res.status(201).json({
+          titles: 'adding url succeed',
+          urlShort,
+        });
+      });
+    });
 
-    getUserId(name).then((data) => {
-      const userId = data.rows[0].id;
-      return addUrlData({
-        urlFull, urlShort, visitors, userId,
-      });
-    }).then(() => {
-      res.status(201).json({
-        titles: 'adding url succeed',
-        urlShort,
-      });
-    }).catch(next);
+    // const visitors = 0;
+
+    // getUserId(name).then((data) => {
+    //   const userId = data.rows[0].id;
+    //   return addUrlData({
+    //     urlFull, urlShort, visitors, userId,
+    //   });
+    // }).then(() => {
+    //   res.status(201).json({
+    //     titles: 'adding url succeed',
+    //     urlShort,
+    //   });
+    // }).catch(next);
   } catch (err) {
     next(err);
   }
